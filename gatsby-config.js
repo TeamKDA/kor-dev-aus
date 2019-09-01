250eb2eecdbec8dd06c3499382a9b7d9ad8561ff
Sujin Lee <sujinlee02@gmail.com>
2019-09-01 21:38:03 +1000

    remove styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
     },  
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
     {
         resolve: `gatsby-transformer-remark`,
         options: {
